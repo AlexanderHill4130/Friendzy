83f1df340fa9acd7093403f33afc9db7a943bfde
Alexander Hill <alexanderhill4130@gmail.com>
2019-06-19 14:54:59 +0200

    search bar added

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :friendships
+  has_many :friends, through: :friendships
   has_many :user_interests
   has_many :interests, through: :user_interests
+
+  include PgSearch
+  pg_search_scope :search_by_first_name_and_last_name,
+                  against: %i[first_name last_name],
+                  using: { tsearch: { prefix: true } }
 end
