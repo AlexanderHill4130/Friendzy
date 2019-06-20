092a52889be84055d220f7232a82f9fa179f0d0a
Paul Valente <paul.val1@hotmail.com>
2019-06-20 22:54:03 +0200

    added join/leave chatroom capability and sorted routes error

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :chatrooms
   devise_for :users
   root to: 'pages#home'
   resources :places, only: :index
@@ -7,4 +6,7 @@ Rails.application.routes.draw do
   resources :friendships
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :places
+  resources :chatrooms do
+    resource :chatroom_users
+  end
 end
